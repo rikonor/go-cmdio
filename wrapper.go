198b77511ea96ad86563ea204b072b8a8f602373
Or Rikon <rikonor@gmail.com>
2018-03-02 15:45:22 -0500

    Elaborate on Wrap function to accept multiples of each

--- wrapper.go
+++ wrapper.go
@@ -1,20 +1,123 @@
 package cmdio
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"syscall"
 
+	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 )
 
 // CloseFunc needs to be called to clean up resources
 type CloseFunc func() error
 
-// Wrap wraps a command such that it uses named pipes attached to the given reader
+// Wrap wraps a command such that it uses named pipes attached to the given readers and writers
+// instead of using actual files.
+func Wrap(rs []io.Reader, ws []io.Writer, execArgs []string) ([]string, CloseFunc, error) {
+	// Keep track of files to remove later
+	toRemove := []string{}
+
+	i := 0
+	getNextName := func() string {
+		i++
+		return fmt.Sprintf("pipe%d", i)
+	}
+
+	eg := &errgroup.Group{}
+
+	for i := 0; i < len(execArgs); i++ {
+		arg := execArgs[i]
+
+		switch {
+		// Handle input file
+		case strings.HasPrefix(arg, "INPUT"):
+			// Get reader index
+			idxStr := strings.TrimPrefix(arg, "INPUT")
+			idx, err := strconv.Atoi(idxStr)
+			if err != nil {
+				return nil, nil, errors.Wrap(err, "failed to parse reader index from arg name")
+			}
+
+			pipeName := getNextName()
+			execArgs[i] = pipeName
+
+			// Create pipe file
+			if err := syscall.Mkfifo(pipeName, 0644); err != nil {
+				return nil, nil, errors.Wrap(err, "failed to create pipe")
+			}
+
+			// Add pipe to removal list
+			toRemove = append(toRemove, pipeName)
+
+			// Start copying from reader to pipe
+			eg.Go(func() error {
+				fIn, err := os.OpenFile(pipeName, os.O_WRONLY, os.ModeNamedPipe)
+				if err != nil {
+					return errors.Wrap(err, "failed to open pipe")
+				}
+				defer fIn.Close()
+
+				_, err = io.Copy(fIn, rs[idx-1])
+				return errors.Wrap(err, "failed to copy from reader to pipe")
+			})
+
+		// Handle output file
+		case strings.HasPrefix(arg, "OUTPUT"):
+			// Get writer index
+			idxStr := strings.TrimPrefix(arg, "OUTPUT")
+			idx, err := strconv.Atoi(idxStr)
+			if err != nil {
+				return nil, nil, errors.Wrap(err, "failed to parse writer index from arg name")
+			}
+
+			pipeName := getNextName()
+			execArgs[i] = pipeName
+
+			// Create pipe file
+			if err := syscall.Mkfifo(pipeName, 0644); err != nil {
+				return nil, nil, errors.Wrap(err, "failed to create pipe")
+			}
+
+			// Add pipe to removal list
+			toRemove = append(toRemove, pipeName)
+
+			// Start copying from pipe to writer
+			eg.Go(func() error {
+				fOut, err := os.OpenFile(pipeName, os.O_RDONLY, os.ModeNamedPipe)
+				if err != nil {
+					return errors.Wrap(err, "failed to open pipe")
+				}
+				defer fOut.Close()
+
+				_, err = io.Copy(ws[idx-1], fOut)
+				return errors.Wrap(err, "failed to copy from pipe to writer")
+			})
+		}
+	}
+
+	// Setup cleanup function
+	closeFn := func() error {
+		// Remove pipe files
+		for _, fPath := range toRemove {
+			if err := os.Remove(fPath); err != nil {
+				return errors.Wrap(err, "failed to remove pipe")
+			}
+		}
+
+		return eg.Wait()
+	}
+
+	return execArgs, closeFn, nil
+}
+
+// WrapSimple wraps a command such that it uses named pipes attached to the given reader
 // and writer instead of using actual files.
-func Wrap(r io.Reader, w io.Writer, execArgs []string) ([]string, CloseFunc, error) {
+func WrapSimple(r io.Reader, w io.Writer, execArgs []string) ([]string, CloseFunc, error) {
 	// Setup pipe names
 	inPipe := "inPipe"
 	outPipe := "outPipe"
